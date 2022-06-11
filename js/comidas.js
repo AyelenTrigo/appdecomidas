c1eff0e8abdbdff811d9b2465948b665f16336fc
Ayelén Trigo <aye.trigo@hotmail.com>
2022-06-11 20:32:36 -0300

    v2 con recetas sin muchos cambios en el script

--- js/comidas.js
+++ js/comidas.js
@@ -1,88 +1,237 @@
 var principal = [
   {
     id: 1,
-    Nombre: "Milanesas",
-    tipo: ["Carne", "Pollo", "Pescado", "Berenjena", "Zuchinni", "Zapallito", "Calabaza"]},
-
+    Nombre: "Milanesas de pollo",
+    tipo: [ "Pollo", "Pechuga", "Pan rallado", "Huevos"],
+    Receta: "Adobar las pechugas con sal y pimienta. En un envase colocar los huevos, el ajo y el perejil picados finamente. Bañar las pechugas con huevo y luego con pan rallado. Freír en abundante aceite caliente hasta dorarlas, durante 2 minutos aproximadamente. Colocarlas sobre papel absorbente para eliminar el exceso de grasa y luego servir."
+  },
   {
     id: 2,
-    Nombre: "Omelette",
-    tipo:[ "Huevos", "Jamón", "Queso", "Aceitunas", "Tomates"]
+    Nombre: "Milanesas de carne",
+    tipo: [ "Carne", "Nalga", "Cuadrada", "Bola de lomo", "Huevos", "Orégano"],
+    Receta: "Sazonar la carne con sal, pimienta y orégano. Una vez sazonada, pasarla por pan rallado; esto nos servirá para rebozarla. Batir los seis huevos. Bañar los filetes en la mezcla y luego pasarlos de nuevo por el pan rallado para asegurar el rebozado. Poner a calentar el aceite en una sartén. Cuando esté bien caliente, freir los filetes por ambos lados hasta que estén dorados. Reservarlos en un plato con papel absorbente para filtrar el exceso de aceite. Colocar la salsa de tomate en un recipiente. Mojar todos los filetes recién fritos en la salsa y colocarlos en una bandeja para hornear. Sobre cada uno de ellos, colocar una tira de jamón y una de queso. Hornear durante 5 minutos a 180 ºC hasta gratinar. Retirarlas y servir."
   },
   {
     id: 3,
-    Nombre: "Hamburguesas",
-    tipo: ["Carne picada","Quinoa","Arroz","Mijo"]
+    Nombre: "Milanesas de berenjenas",
+    tipo: [ "Berenjenas", "Ajo", "Huevos", "Perejil", "Pan rallado", "Queso parmesano", "Zapallitos", "Zuchinnis", "Calabaza"],
+    opcionales: ["Zapallitos", "Zuchinnis", "Calabaza"],
+    Receta: "Pela la berenjena y córtala en rebanadas, a lo largo, de 1 cm cada una; agrégales sal para quitar el amargor. En un bol con una rejilla, coloca las rebanadas de berenjena con la sal para que suelte su jugo durante 20 minutos. Seca muy bien las berenjenas con un papel absorbente. En un bol, mezcla el huevo con el ajo previamente picado. En otr bol, mezcla el pan molido con el queso parmesano y el perejil. Para empanizar, pasa una rebanada de berenjena por la mezcla del huevo y después por la mezcla del pan. Realiza estos dos procesos dos veces. Calienta el aceite y fríe las berenjenas por 4 minutos de cada lado, luego déjalas en un plato con papel absorbente para retirar el exceso de aceite. Servir en un plato extendido y acompañar con arroz poblano."
   },
   {
     id: 4,
-    Nombre: "Pastas",
-    tipo: ["Fideos de sémola","Fideos de zucchini","Ñoquis de papa","Ñoquis de calabaza"]
+    Nombre: "Omelette",
+    tipo: ["Huevos"],
+    opcionales: ["Jamón", "Queso", "Aceitunas", "Tomates"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
   {
     id: 5,
+    Nombre: "Hamburguesas",
+    tipo: ["Carne picada", "Quinoa", "Arroz", "Mijo", "Lentejas"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
+  },
+  {
+    id: 6,
+    Nombre: "Pastas",
+    tipo: ["Sémola", "Zucchini", "Ñoquis de papa", "Ñoquis de calabaza"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
+  },
+  {
+    id: 7,
     Nombre: "Tarta",
-    tipo: ["Jamón","Queso","Atún","Pollo","Zapallitos","Puerro","Calabaza","Espinaca","Choclo","Acelga","Brócoli"]}
+    tipo: [ "Jamón y queso", "Atún", "Pollo", "Zapallitos", "Puerro", "Calabaza", "Espinaca"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
+  },
+  {
+    id: 8,
+    Nombre: "Albóndigas",
+    tipo: ["Carne picada", "Arroz", "Soja"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
+  },
+  {
+    id: 9,
+    Nombre: "Buñuelos",
+    tipo: ["Espinaca", "Acelga", "Zanahoria", "Hojas de remolacha"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
+  },
+  {
+    id: 10,
+    Nombre: "Salteado de verduras",
+    tipo: [ "Cebolla", "Morrón", "Zanahoria", "Brotes de soja", "Zuchinni", "Berenjenas", "Zapallitos"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
+  },
+  {
+    id: 11,
+    Nombre: "Tarta de Acelga",
+    tipo: [ "Acelga", "Ajo", "Huevos", "Cebolla", "Queso rallado", "Tapa de tarta"],
+    opcionales: ["Queso fresco"],
+    Receta: "Lavas las acelgas, cortas el tallo de las hojas y  las colocas en una olla grande sin agua. El agua de la acelga es suficiente. Pones a fuego mínimo las acelgas por un momento, hasta que se cocine un poco, y se tornen blandas. Las retiras del fuego. Cuelas las acelgas y las dejas enfriar, mientras se le escurre toda el agua. Una vez estén frías, tomas porciones de las acelgas y presiónalas entre tus manos para exprimir el agua restante. Picas las acelgas finamente y déjalas en un lado. Cortas la cebolla y el ajo en pedazos pequeños. Salteas la cebolla hasta que se torne transparente, después agregas un poco de ajo. Añade las acelgas picadas y salteas hasta que las acelgas estén completamente secas y hayan tomado el sabor de la preparación. Retiras del fuego y dejas enfriar para utilizarlas posteriormente en la mezcla de la tarta. Mezclas los dos huevos en un envase. En un envase grande, mezclas la preparación de acelga con cebolla y ajo, agregas el queso rallado, pimienta, sal y la nuez moscada a tu gusto. Agregas los huevos a la mezcla anterior y mezclas. Preparas un molde de tartas o una fuente. Aceitas la fuente y colocas la tapa de tarta en el fondo, estírala si es necesario. Colocas el relleno bien distribuido en toda la fuente o tartera. Puedes colocar la tapa de tarta superior o no. Calientas el horno.  Introduces la tarta en el horno, durante 30 o 40 minutos hasta que la masa quede dorada. Puedes agregar trozos de queso fresco o huevos cocidos en el relleno para añadir sabor"
+  },
+  {
+    id: 12,
+    Nombre: "Tarta de choclo, queso y cebolla",
+    tipo: [ "Cebolla", "Choclo", "Huevo", "Queso cremoso", "Queso rallado", "Tapa de tarta"],
+    Receta: "Forras una tartera con la tapa de tarta, cubres con papel manteca o metalizado y horneas a 180° por 20 minutos. Salteas la cebolla, mezclas con el choclo y el huevo y salpimientas, vuelcas todo sobre la base de la tarta una vez sacada del horno. Pones encima una rodaja generosa de queso cremoso y dejas cocinar en horno de 200° unos 15min. Al servir espolvoreas con queso rallado."
+  },
+  {
+    id: 13,
+    Nombre: "Tarta de puerros y zapallitos",
+    tipo: [ "Zapallitos", "Puerros", "Huevos", "Queso crema", "Queso rallado", "Tapa de tarta"],
+    Receta: "En una sartén con una cucharada de aceite colocar los puerros cortados, sofreír unos minutos e incorporar los zapallitos o zucchinis previamente cortados en tiritas. Cuando los zapallitos estén tiernos ya estarán listos. Precalentar el horno a 170º, estirar la masa y disponerla en un molde para el horno. Pichar la superficie y hornear por 10 minutos. Aparte batir los huevos, añadir sal y pimienta, incorporar el queso crema y el queso rallado, mezclar bien y a continuación añadir esta mezcla a los puerros y zapallitos cocidos. Verter este relleno sobre la masa precocida  y llevar a horno por unos 20 minutos más. Apagar cuando el huevo haya cuajado y la superficie de la misma presente un color ligeramente dorado."
+  },
+  {
+    id: 14,
+    Nombre: "Tarta de brócoli",
+    tipo: [ "Brócoli", "Ricota", "Queso fresco", "Tapa de tarta"],
+    opcionales: ["Queso port salut", "Queso Mozarella"],
+    Receta: "Cocinar el brócoli hasta que el brócoli quede tierno pero no se desarme. Cortar la cebolla en juliana y rehogar en un sartén con las cucharadas de aceite, cuando comiencen a estar transparentes. Cortar el brócoli en pedazos pequeños y disponer en un bol grande, agregamos la ricota, salpimentar a gusto, añadir la cebolla, mezclar para que se integren todos los ingredientes. Disponer el relleno sobre la masa precocida de manera uniforme, y esparcir el queso elegido que puede ser en tiras o rallado. Terminar la cocción de la tarta de Brócoli por unos 20 minutos aproximadamente hasta observar que el queso comienza a fundirse y dorarse"
+  },
+  {
+    id: 15,
+    Nombre: "Medallones de legumbres",
+    tipo: [ "Garbanzos", "Lenjetas", "Porotos", "Arvejas", "Cebolla", "Ajo", "Caldo vegetal", "Avena en copos", "Harina de avena"],
+    opcionales: ["Garbanzos", "Lenjetas", "Porotos", "Arvejas"],
+    Receta: "Picamos finamente la cebolla y los dos ajos y saltar en una sartén con aceite de oliva. Cuando estén dorados, volcamos a un bol y añadimos las legumbres cocidas, una pizca de sal, abundante pimienta negra, albahaca, un trocito de cilantro picado, 1 cdita pequeña de pimentón dulce y un chorrito de lima. Mezclamos bien. Añadimos una taza de avena en copos y un chorrito de caldo vegetal. Mezclamos y trituramos con una batidora de mano hasta conseguir una textura homogénea y cremosa. Añadimos ¾ de taza de harina de avena y mezclamos de nuevo, hasta que la masa haya espesado y sea fácilmente manejable. Volcamos el resto de harina de avena en un plato hondo y reservamos. Hacemos bollitos de masa y le damos forma de medallón. Puedes hacerlo tan fino o grueso como desees. Rebozar en la harina de avena y reservar en un plato limpio. Repetiremos el proceso hasta terminar con toda la masa. En una sartén, calentamos un chorrito largo de aceite de oliva y, una vez caliente, doramos los medallones por ambos costados. Cuando estén bien hechos, servimos caliente."
+  },
 ];
 var acompañamiento = [
   {
     id: 1,
     Nombre: "Salsa Filetto",
-    ing: ["Tomate","Ajo","Albahaca","Ketchup"]
+    ing: ["Tomate", "Ajo", "Albahaca", "Ketchup"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
   {
     id: 2,
     Nombre: "Salsa Blanca",
-    ing: ["Harina","Leche","Manteca","Maicena"]
+    ing: ["Harina", "Leche", "Manteca", "Maicena"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
   {
     id: 3,
     Nombre: "Crema",
-    ing: ["Crema","Cebolla","Cebolla de verdeo","Champiniones","Aceitunas negras","Leche"]
+    ing: [ "Crema", "Cebolla", "Cebolla de verdeo", "Champiniones", "Aceitunas negras", "Leche" ],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
   {
     id: 4,
     Nombre: "Salsa de queso",
-    ing: ["Queso azul","Leche","Maicena","Cebolla","Queso en hebras"]
+    ing: ["Queso azul", "Leche", "Maicena", "Cebolla", "Queso en hebras"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
   {
     id: 5,
     Nombre: "Salsa de puerro y verdeo",
-    ing: ["Puerros","Cebolla de verdeo","Ajo","Crema","Queso en hebras"]
-  },{
+    ing: ["Puerros", "Cebolla de verdeo", "Ajo", "Crema", "Queso en hebras"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
+  },
+  {
     id: 6,
     Nombre: "Papas al horno",
     ing: ["Papas", "Oregano", "Pimentón ahumado"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
   {
     id: 7,
     Nombre: "Puré",
     ing: ["Papas", "Leche", "Manteca"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
   {
     id: 8,
     Nombre: "Papas fritas",
-    ing:[ "Papas", "Oregano", "Ajo", "Perejil"],
+    ing: ["Papas", "Oregano", "Ajo", "Perejil"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
   {
     id: 9,
     Nombre: "Ensalada rusa",
-    ing: ["Papas","Huevo","Zanahoria","Arvejas","Mayonesa",]
+    ing: ["Papas", "Huevos", "Zanahoria", "Arvejas", "Mayonesa"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
   {
     id: 10,
-    Nombre: "Ensalada",
-    ing: ["Tomate","Lechuga","Zanahoria","Cebolla","Choclo","Palta","Manzana","Rúcula","Radicheta","Ajo","Limón","Pollo"]
+    Nombre: "Ensalada de zanahoria y choclo",
+    ing: [ "Zanahoria", "Cebolla", "Choclo", "Limón", "Huevos" ],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
   },
-  
   {
     id: 11,
     Nombre: "Guacamole",
-    ing:[ "Palta","Morrón","Cebolla","Tomate","Limón"]
+    ing: ["Palta", "Ají jalapeño", "Cebolla", "Tomates", "Lima", "Aceite"],
+    Receta: "Pelar las paltas y hacerlas puré con un tenedor. Ponerles unas gotas de jugo de lima o de limón y dejarles un carozo para que no se oscurezcan. Pelar y picar muy finamente la cebolla. Picar chiquito el tomate. Quitar las semillas al ají y picarlo muy finito. Mezclar el puré de palta con la cebolla, el jalapeño, el cilantro y el tomate, condimentar con sal y aceite. Si se debe guardar para servir luego, cubrir bien con papel foil y llevarlo a la heladera."
   },
   {
     id: 12,
     Nombre: "Ensalada capresse",
-    ing: ["Tomate","Albahaca","Queso mozzarella"]
-  }
+    ing: ["Tomates", "Albahaca", "Queso mozzarella"],
+    Receta: "Cortar la mozzarella en rodajas de un centímetro de espesor. Cortar los tomates en rodajas parejas de un centímetro de espesor.En una fuente o plato de servir, intercalar las rodajas de mozzarella con las rodajas de tomate. Entre los dos ingredientes, poner una hojita de albahaca.Rociar con aceite de oliva, sal y pimienta."
+  },
+  {
+    id: 13,
+    Nombre: "Ensalada griega",
+    ing: ["Lechuga", "Tomates", "Pepinos", "Morrón verde", "Cebolla colorada", "Aceitunas negras", "Queso feta"],
+    Receta: "Pelar el pepino y cortarlo en rodajas finas. Lavar y secar la lechuga. Cortar la lechuga en trozos con las manos. Lavar el tomate y cortarlo en gajos. Pelar la cebolla y cortarla en rodajas finas. Descartar las semillas y nervaduras del morrón y cortarlo en tiras. Cortar el queso en cubos. Mezclar en una ensaladera la lechuga, los tomates, el ají, el pepino, el queso y las aceitunas. Hacer una vinagreta con el aceite, el jugo de limón, la sal y la pimienta. Condimentar la ensalada con la vinagreta y servir."
+  },
+  {
+    id: 14,
+    Nombre: "Ensalada coleslaw",
+    ing: ["Col blanca", "Zanahoria", "Manzana", "Mayonesa", "Crema agria", "Cebolla", "Mostaza de Dijon"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
+  },
+  {
+    id: 15,
+    Nombre: "Ensalada caesar",
+    ing: ["Lechuga", "Croutons", "Pollo", "Salsa inglesa", "Queso parmesano", "Limón", "Aceite"],
+    Receta:"Cortar las hojas de lechuga, como para hacer una ensalada común, con las manos. Ir poniendo la lechuga en el bowl con la salsa mientras se revuelve de forma envolvente para que se impregnen bien todas las hojas. Intercalar con el aceite y el jugo de limon. Agregar el pollo y mezclar. Poner los croutons en el centro de la ensalada. Servir acompañada de queso rallado"
+  },
+  {
+    id: 16,
+    Nombre: "Ensalada criolla",
+    ing: ["Morrón rojo", "Morrón verde", "Cebolla", "Tomates", "Aceite", "Vinagre"],
+    Receta: "Lavar y secar los vegetales. Quitar las nervaduras y semillas a los morrones. Pelar la cebolla. Picar grueso los ajíes y las cebollas. Cortar en cubitos muy pequeños los tomates En un bol, mezclar bien el aceite con el vinagre. Aderezar con la mezcla de aceite y vinagre la ensalada criolla. Salar a gusto. Dejar descansar unos 15 minutos antes de servir."
+  },
+  {
+    id: 17,
+    Nombre: "Ensalada de arroz y atún",
+    ing: ["Arroz", "Morrón", "Apio", "Atún", "Choclo", "Aceite", "Mayonesa"],
+    Receta: "Hervir el arroz en abundante agua con sal. Cuando esté a punto (entre 15 y 20 minutos) colar y dejar enfriar. Untar el ají con aceite. Poner el ají en horno moderado hasta que la piel se arrugue y se desprenda de la pulpa (media hora aproximadamente), girándolos de vez en cuando. Sacar el ají del horno, dejar que se enfríe y pelarlo Abrirlo al medio, desechar las semillas y cortarlo en trocitos de 1 cm. de lado más o menos. Abrir la lata de choclos y escurrirlos. Cortar picar el apio algo grueso. Cortar el atún en trozos. Mezclar los ingredientes y condimentarlos con sal y aceite o con mayonesa."
+  },
+  {
+    id: 18,
+    Nombre: "Ensalada waldorf",
+    ing: ["Manzanas verdes", "Apio", "Mayonesa","Nueces", "Limón", "Vinagre"],
+    Receta: "Próximamente vas a tener esta receta detallada aquí"
+  },
+  {
+    id: 19,
+    Nombre: "Ensalada verde",
+    ing: ["Lechuga", "Espinaca", "Rúcula", "Aceite", "Limón"],
+    Receta: "Lavar los vegetales, sumergiéndolos en abundante agua. Cambiar el agua al menos 2 veces. Escurrir y secar muy bien las hojas con un paño. Cortar las hojas de lechuga y espinaca con las manos. Las hojas de rúcula si no son muy grandes van enteras. Hacer una vinagreta mezclando el aceite, el jugo de limón y la sal. Mezclar las hojas con la vinagreta en el momento de servir."
+  },
+  {
+    id: 20,
+    Nombre: "Ensalada mixta",
+    ing: ["Lechuga", "Tomates", "Cebollas", "Aceite", "Vinagre"],
+    Receta: "Lavar muy bien la lechuga, se puede sumergir en abundante agua limpia e ir limpiando las hojas y cambiando el agua varias veces. Sacudir muy bien las hojas de lechuga, tomándolas de a pocas, para tratar de quitarles la mayor cantidad de agua posible. Cortar con la mano las hojas de lechuga del tamaño de un bocado pequeño. Lavar los tomates y cortarlos en medias rodajas de más o menos 3 mm de ancho. Pelar la cebolla y cortarla en medias rodajas finas. Colocar toda la verdura en una ensaladera. Mezclar el aceite con el vinagre y aderezar la ensalada mixta justo antes de servir. Salar a gusto y mezclar muy bien."
+  },
+  {
+    id: 21,
+    Nombre: "Ensalada de zanahoria y huevo",
+    ing: ["Zanahoria", "Huevos", "Limón", "Aceite", "Azúcar"],
+    Receta: "Pelar las zanahorias. Rallar las zanahorias con la parte fina del rallador (agrega un poco de trabajo, si no se tiene procesadora, pero quedan más ricas que ralladas grueso). Agregar el azúcar. Revolver y dejar descansar un ratito para que la zanahoria absorba bien el azúcar. Hacer los huevos duros cocinándolos 10 minutos en agua hirviendo. Pasar los huevos por agua fría, pelarlos y cortarlos en cuartos. A último momento condimentar con aceite, jugo de limón y sal. Incorporar los huevos y servir la ensalada."
+  },
+  {
+    id: 22,
+    Nombre: "Ensalada de zanahoria y apio",
+    ing: ["Zanahoria", "Apio", "Limón", "Aceite", "Azúcar"],
+    Receta: "Pelar las zanahorias y rayarlas finitas. Lavar el apio y cortarlo en trocitos muy chicos. Mezclar la zanahoria con el apio y condimentar con unas gotas de edulcorante o azúcar, sal, jugo de limón y dos toques de aceite en spray o una cucharada de aceite de oliva."
+  },
+  {
+    id: 23,
+    Nombre: "Ensalada de rúcula y pera",
+    ing: ["Rúcula", "Peras", "Limón", "Aceite", "Queso azul", "Nueces"],
+    Receta: " Lavar bien la rúcula y quitar las raíces. La rúcula debe estar bien fresca, si está un poco mustia se puede colocar 15 minutos en un bol con agua bien fría para que se hidrate. Colocar en un colador para que se seque, o colocar entre dos repasadores limpios, sin apretar. Pelar la pera, cortar a la mitad y quitar las semillas. Cortar la pera en láminas no muy finas y rociar con el limón para que no se oscurezca. Cortar en trozos pequeños el queso. En una ensaladera, colocar la rúcula. Sobre ella, ubicar las láminas de pera, las nueces y, al terminar, el queso. En un bol pequeño, mezclar sal a gusto, pimienta, aceto balsámico y aceite hasta emulsionar. Rociar la ensalada con la vinagreta. No mezclar demasiado para que el queso no se deshaga."
+  },
 ];
--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 //BOTONES
 let random = document.getElementById("random");
 let heladera = document.getElementById("heladera");
-let heladera2 = document.getElementById("heladera2");
+let volver = document.getElementById("volver");
+let otroRandom = document.getElementById("otroRandom");
 let otroPlato = document.getElementById("otroPlato");
+let verComidas = document.getElementById("verComidas");
 
 //PANTALLAS
 let paginaComidas = document.getElementById("paginaComidas");
@@ -17,15 +19,24 @@ let ingredientes = document.getElementById("ingredientes");
 let producto1 = document.getElementById("producto1");
 let producto2 = document.getElementById("producto2");
 let plato = document.getElementById("plato");
+let validacion = document.getElementById("validacion");
+let opcionesPrincipal=[]
+let opcionesAcompañamiento=[]
 
 random.addEventListener("click", () => {
+  opcionesPrincipal=[]
+  opcionesAcompañamiento=[]
+  resetOpciones()
   platoRandom();
+  otroRandom.style.display = "block";
+  otroPlato.style.display = "none";
   paginaComidas.style.display = "block";
   paginaHeladera.style.display = "none";
   inicio.style.display = "none";
 });
 
-otroPlato.addEventListener("click", () => {
+otroRandom.addEventListener("click", () => {
+  resetOpciones();
   platoRandom();
   paginaComidas.style.display = "block";
   paginaHeladera.style.display = "none";
@@ -38,54 +49,140 @@ heladera.addEventListener("click", () => {
   inicio.style.display = "none";
 });
 
-heladera2.addEventListener("click", () => {
-  paginaHeladera.style.display = "block";
+volver.addEventListener("click", () => {
+  paginaHeladera.style.display = "none";
   paginaComidas.style.display = "none";
-  inicio.style.display = "none";
+  inicio.style.display = "block";
 });
+
 
 const platoRandom = () => {
   resetOpciones();
-
+  opcionesPrincipal=[]
+  opcionesAcompañamiento=[]
   let randomP = Math.floor(Math.random() * principal.length);
-
   let randomA = Math.floor(Math.random() * (acompañamiento.length - 5) + 5);
 
   if (principal[randomP].Nombre != "Pastas") {
     plato.innerHTML = `${principal[randomP].Nombre} con ${acompañamiento[randomA].Nombre}`;
 
     console.log(
-      [randomP],principal[randomP].Nombre,"con",[randomA],acompañamiento[randomA].Nombre);
-
+      [randomP],
+      principal[randomP].Nombre,
+      "con",
+      [randomA],
+      acompañamiento[randomA].Nombre
+    );
   } else {
     randomA = Math.floor(Math.random() * 5);
     //pongo *5 porque son 5 elementos
     plato.innerHTML = `${principal[randomP].Nombre} con ${acompañamiento[randomA].Nombre}`;
 
-    console.log([randomP],principal[randomP].Nombre,"con",[randomA],acompañamiento[randomA].Nombre);
+    console.log(
+      [randomP],
+      principal[randomP].Nombre,
+      "con",
+      [randomA],
+      acompañamiento[randomA].Nombre
+    );
   }
-  
-  let opcionesPrincipal = principal[randomP].tipo;
-  let opcionesAcompañamiento = acompañamiento[randomA].ing;
 
-  imprimirOpciones(opcionesPrincipal,opcionesAcompañamiento);
+  opcionesPrincipal = principal[randomP].tipo;
+  opcionesAcompañamiento = acompañamiento[randomA].ing;
+
+  imprimirPrincipal(opcionesPrincipal)
+  imprimirAcompañamiento(opcionesAcompañamiento)
+};
+
+let mainDish;
+let sideDish;
+let arrayMain = [];
+let arraySide = [];
+let randomMain;
+let randomSide;
+
+function elegirIngredientes() {
+  opcionesPrincipal=[];
+  opcionesAcompañamiento=[];
+  principal.forEach((objeto) => {
+    for (let i = 0; i < objeto.tipo.length; i++) {
+      let element = objeto.tipo[i];
+      if (element == producto1.value || element == producto2.value) {
+        mainDish = objeto.Nombre;
+        arrayMain.push(mainDish);
+        opcionesPrincipal.push(objeto.tipo)
+      }
+    }
+  });
+  acompañamiento.forEach((objeto) => {
+    for (let i = 0; i < objeto.ing.length; i++) {
+      let element = objeto.ing[i];
+      if (element == producto1.value || element == producto2.value) {
+        sideDish = objeto.Nombre;
+        arraySide.push(sideDish);
+        opcionesAcompañamiento.push(objeto.ing)
+      }
+    }
+  });
+    console.log(arraySide)
+
+  randomMain = Math.floor(Math.random() * arrayMain.length);
+  randomSide = Math.floor(Math.random() * arraySide.length);
+
+  console.log(arrayMain[randomMain], "con", arraySide[randomSide]);
+  plato.innerHTML = `${arrayMain[randomMain]} con ${arraySide[randomSide]}`;
+
+  opcionesPrincipal=opcionesPrincipal[randomMain]
+  opcionesAcompañamiento=opcionesAcompañamiento[randomSide]
+
+  imprimirPrincipal(opcionesPrincipal)
+  imprimirAcompañamiento(opcionesAcompañamiento);
 }
-
-function imprimirOpciones(opcionesPrincipal,opcionesAcompañamiento){
-
+function imprimirPrincipal(){
   for (let i = 0; i < opcionesPrincipal.length; i++) {
     let opcionP = document.createElement("li");
     let txt = document.createTextNode(opcionesPrincipal[i]);
     opcionP.appendChild(txt);
     listaOpciones.appendChild(opcionP);
   }
-  for (let i = 0; i < opcionesAcompañamiento.length; i++) {
-    let opcionA = document.createElement("li");
-    let txt = document.createTextNode(opcionesAcompañamiento[i]);
-    opcionA.appendChild(txt);
-    ingredientes.appendChild(opcionA);
-  }
 }
+function imprimirAcompañamiento() {
+    for (let i = 0; i < opcionesAcompañamiento.length; i++) {
+      let opcionA = document.createElement("li");
+      let txt = document.createTextNode(opcionesAcompañamiento[i]);
+      opcionA.appendChild(txt);
+      ingredientes.appendChild(opcionA);
+    }
+}
+
+verComidas.addEventListener("click", (e) => {
+  e.preventDefault();
+  resetOpciones();
+  opcionesPrincipal=[];
+  opcionesAcompañamiento=[];
+  if (producto1.value == "" && producto2.value == "") {
+    validacion.style.display = "block";
+  } else {
+    elegirIngredientes();
+    validacion.style.display = "none";
+    otroPlato.style.display = "block";
+    otroRandom.style.display = "none";
+    paginaComidas.style.display = "block";
+    paginaHeladera.style.display = "none";
+    inicio.style.display = "none";
+  }
+});
+
+otroPlato.addEventListener("click", (e) => {
+  e.preventDefault();
+  resetOpciones();
+  arrayMain = [];
+  arraySide = [];
+  opcionesPrincipal=[];
+  opcionesAcompañamiento=[];
+  
+  elegirIngredientes();
+});
 
 function resetOpciones() {
   if (listaOpciones.hasChildNodes()) {
